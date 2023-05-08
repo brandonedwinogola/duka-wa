e88c39e79459a1f315d4e4153136b97b45ecf4b1
brandonedwinogola <brandon14ogola@gmail.com>
2023-05-08 18:49:30 +0300

    tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,44 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      width:{
+        'inherit': 'inherit',
+      },
+      screens:{
+        'sm': '470px'
+      },
+      fontFamily:{
+        'open-sans': ['Open Sans', ...defaultTheme.fontFamily.sans],
+        'source-sans-pro': ['Source Sans Pro', ...defaultTheme.fontFamily.sans]
+      },
+      padding:{
+        '0.5': '2px'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant, e, postcss}){
+      addVariant('firefox', ({ container, separator }) => {
+        const isFirefoxRule = postcss.atRule({
+          name: '-moz-document',
+          params: 'url-prefix()',
+        })
+        isFirefoxRule.append(container.nodes);
+        container.append(isFirefoxRule);
+        isFirefoxRule.walkRules((rule) => {
+          rule.selector = `.${e(
+            `firefox${separator}${rule.selector.slice(1)}`
+          )}`
+        })
+      })
+    })
+  ],
 }
 
